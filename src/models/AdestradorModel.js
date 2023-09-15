c8b12e300479046c61380edb08b8a1cbed9dc9cb
Thayse342 <thayse342@gmail.com>
2023-09-15 11:05:46 -0300

    feat: :sparkles: Criação da entidade  adestrador
    Feito a modelagem e criado a entidade adestrador

---
+++ src/models/AdestradorModel.js
@@ -0,0 +1,19 @@
+class AdestradorModel {
+
+    /**
+     * 
+     * @param {number} id 
+     * @param {string} formacao 
+     * @param {string} status 
+     * @param {string} pessoachavestrangeira 
+     */
+    
+        constructor(id,formacao,status,pessoachavestrangeira){
+            this.id= id
+            this.formacao= formacao
+            this.status= status
+            this.pessoachavestrangeira= pessoachavestrangeira
+        }
+    }
+    
+    export default AdestradorModel
