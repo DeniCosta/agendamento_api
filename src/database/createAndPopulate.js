f745e8438b3a57d85bd5c0d7fc8a9b7ca295882a
Thayse342 <thayse342@gmail.com>
2023-09-19 22:38:39 -0300

    feat: :sparkles: Criado e populado tabela adestrador

--- src/database/createAndPopulate.js
+++ src/database/createAndPopulate.js
@@ -56,3 +56,65 @@ Database.serialize(()=>{
     criaTabelaClientes();
     populaTabelaClientes();
 });
+
+import Database from "./Database.js";
+
+/**
+ * Script sql de criação da tabela Adestrador 
+ */
+const ADESTRADOR_TABLE = `
+CREATE TABLE IF NOT EXISTS "Adestrador" (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    "NOME" varchar(64) ,
+    "FORMACAO" varchar(64),
+    "ID_AGENDAMENTO"INTEGER, 
+    "ID_Endereco"INTEGER , 
+);
+`
+
+/**
+ * script de inserção de dados base
+ */
+const ADD__DATA = `
+INSERT INTO Adestrador (nome,formacao,id_agendamento,id_endereco)
+VALUES 
+    ('DA Cruz','Auxiliar de veterinário',1,1),
+    (',
+
+`
+
+/**
+ * Function que aplica a criação da tabela Adestrador via SQLite
+ */
+function criaTabelaAdestrador() {
+    Database.run(ADESTRADOR_TABLE, (error) => {
+        if (error) {
+            console.log("Erro ao criar tabela de adestrador")
+        } else {
+            console.log("Tabela Adestrador criada!")
+        }
+    });
+}
+
+/**
+ * Function que polula via SQLite a tabela ADESTRADOR
+ */
+function populaTabelaAdestrador() {
+    Database.run(ADD_ADESTRADOR_DATA, (error) => {
+        if (error) {
+            console.log("Erro ao popular tabela de adestrador")
+        }
+        else {
+            console.log("Tabela Adestrador populada !")
+        }
+    });
+}
+
+
+/**
+ * Roda as funções de criação de tabela e população em serie (Uma após a outra)
+ */
+Database.serialize(() => {
+    criaTabelaAdestrador();
+    populaTabelaAdestrador();
+})
