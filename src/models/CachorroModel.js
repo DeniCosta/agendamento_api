df24e0d76d05cbaa6a7916d23e116e534a03cfe3
IgorSPinto <igorpinto103@gmail.com>
2023-09-21 00:40:16 -0300

    fix: :bug: Remoção de atributos da model

--- src/models/CachorroModel.js
+++ src/models/CachorroModel.js
@@ -1,29 +1,19 @@
 class CachorroModel {
 
   /**
-   * 
-   * @param {number} id 
    * @param {string} nome 
    * @param {string} raca 
    * @param {string} cor 
    * @param {string} sexo 
-   * @param {string} porte 
    * @param {string} peso 
-   * @param {string} temperamento 
-   * @param {boolean} statusVacina 
-   * @param {string} proprietario fk
    */
 
-  constructor(id, nome, raca, cor, sexo, porte, peso, temperamento, statusVacina) {
-    this.id = id
+  constructor(nome, raca, cor, sexo, peso) {
     this.nome = nome
     this.raca = raca
     this.cor = cor
     this.sexo = sexo
-    this.porte = porte
     this.peso = peso
-    this.temperamento = temperamento
-    this.statusVacina = statusVacina
   }
 }
 
